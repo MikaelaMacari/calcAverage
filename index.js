feb73417f4c4c218798dbf9834d105f83754e6dc
Mihaela Macari <mikaela.malanici@gmail.com>
2022-01-21 13:42:56 +0200

    added a comment

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//this is a function that calculate average of a numbers from an array
 const average = function (numbers) {
   return (numbers.reduce((a, b) => a + b, 0) / numbers.length).toFixed(2);
 };
